df83764d9b87b9f326a62ebf2a2779f01ba5a766
TopGitSan <c.toparcean@gmail.com>
2019-04-08 11:21:02 +0200

    remove the powered by header

--- myApp.js
+++ myApp.js
@@ -30,7 +30,7 @@ const helmet = require('helmet');
 // people off. e.g. `helmet.hidePoweredBy({ setTo: 'PHP 4.2.0' })`
 
 // Use `helmet.hidePoweredBy()``
-
+app.use(helmet.hidePoweredBy({setTo: 'Python 3.7.1'}));
 
 
 /** 3) Mitigate the risk of clickjacking - `helmet.frameguard()` */
